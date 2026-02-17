4bb4171ac400f99988673da6e246b42617d70254
Balint Szikora <chamaselion@gmail.com>
2026-02-17 16:25:12 +0100

    COrrected makefile and added extra large test

--- ex01/sources/main.cpp
+++ ex01/sources/main.cpp
@@ -6,13 +6,14 @@
 /*   By: bszikora <bszikora@student.42helbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 14:33:58 by bszikora          #+#    #+#             */
-/*   Updated: 2025/11/27 19:39:58 by bszikora         ###   ########.fr       */
+/*   Updated: 2026/02/17 15:46:38 by bszikora         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Span.hpp"
 #include <iostream>
 #include <vector>
+#include <set>
 #include <cstdlib>
 #include <ctime>
 
@@ -42,6 +43,52 @@ std::cout << "Added range of 5 numbers" << std::endl;
 std::cout << "Span size: " << sp2.size() << std::endl;
 std::cout << "Shortest span: " << sp2.shortestSpan() << std::endl;
 std::cout << "Longest span: " << sp2.longestSpan() << std::endl;
+
+ // Large-scale addRange test: 10,000 numbers
+ std::cout << "\n=== Testing addRange with 10,000 numbers ===" << std::endl;
+ try
+ {
+	const unsigned int N = 10000;
+	Span sp3(N);
+	std::vector<int> big;
+	big.reserve(N);
+	for (unsigned int i = 0; i < N; ++i)
+		big.push_back(static_cast<int>(i));
+
+	sp3.addRange(big.begin(), big.end());
+	std::cout << "Added range of " << N << " numbers" << std::endl;
+	std::cout << "Span size: " << sp3.size() << ", maxSize: " << sp3.maxSize() << std::endl;
+	std::cout << "Shortest span: " << sp3.shortestSpan() << std::endl;
+	std::cout << "Longest span: " << sp3.longestSpan() << std::endl;
+ }
+ catch (const std::exception& e)
+ {
+	std::cout << "Exception during 10,000 addRange test: " << e.what() << std::endl;
+ }
+
+	 // Source from std::set
+	 std::cout << "\n=== Testing addRange from std::set ===" << std::endl;
+	 try
+	 {
+		 Span sp4(6);
+		 std::set<int> s;
+		 s.insert(42);
+		 s.insert(17);
+		 s.insert(17); // duplicate ignored by set
+		 s.insert(99);
+		 s.insert(3);
+		 s.insert(58);
+
+		 sp4.addRange(s.begin(), s.end());
+		 std::cout << "Added " << s.size() << " unique numbers from set" << std::endl;
+		 std::cout << "Span size: " << sp4.size() << ", maxSize: " << sp4.maxSize() << std::endl;
+		 std::cout << "Shortest span: " << sp4.shortestSpan() << std::endl;
+		 std::cout << "Longest span: " << sp4.longestSpan() << std::endl;
+	 }
+	 catch (const std::exception& e)
+	 {
+		 std::cout << "Exception during set addRange test: " << e.what() << std::endl;
+	 }
 
 return 0;
 }
